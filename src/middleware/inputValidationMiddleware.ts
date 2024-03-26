4078611b4521e6889f4a2c56d0d874f28d3cc0f8
7erp1c <ul_tray@bk.ru>
2024-03-26 21:19:29 +0300

    New project lesson 05

--- src/middleware/inputValidationMiddleware.ts
+++ src/middleware/inputValidationMiddleware.ts
@@ -40,7 +40,7 @@ export const blogPostValidation = [
     //     .matches(new RegExp("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z$"))
 ]
  export const usersValidation =[
- body('login').trim().isString().isLength({min:3,max:30}).bail(),
+ body('login').trim().isString().isLength({min:3,max:10}).bail(),
  body('password').trim().isString().isLength({min:6,max:20}).matches(new RegExp("^[a-zA-Z0-9_-]*$")).bail(),
  body('email').trim().isString()
      .matches(new RegExp("^[\\w\\.\\-]+@[\\w\\.\\-]+\\.[a-zA-Z]{2,4}$")).bail()
