7f6bcf3a7c5a5d1d7ac363aef505dc3c1f1b28ca
7erp1c <ul_tray@bk.ru>
2024-03-26 22:00:53 +0300

    New project lesson 05

--- src/middleware/inputValidationMiddleware.ts
+++ src/middleware/inputValidationMiddleware.ts
@@ -1,4 +1,4 @@
-import {body} from "express-validator";
+import {body, check, oneOf} from "express-validator";
 import { blogCollection } from "../db/mongo-db";
 
 
@@ -47,8 +47,24 @@ export const blogPostValidation = [
  ]
 
 export const authValidation = [
-    body('loginOrEmail').trim().isString().bail(),
-    body('password').trim().isString().isLength({min:6,max:20}).matches(new RegExp("^[a-zA-Z0-9_-]*$")).bail()
+    oneOf([
+        check('loginOrEmail').trim().isString().isLength({min:3,max:10}).bail(),
+        check('loginOrEmail').trim().isString()
+            .matches(new RegExp("^[\\w\\.\\-]+@[\\w\\.\\-]+\\.[a-zA-Z]{2,4}$")).bail()
+    ]),
+
+    // body('loginOrEmail').custom((value,{req})=>{
+    //     if(value.includes('@')){
+    //         req.checkBody('loginOrEmail').trim().isString()
+    //              .matches(new RegExp("^[\\w\\.\\-]+@[\\w\\.\\-]+\\.[a-zA-Z]{2,4}$")).bail()
+    //
+    //     }else{
+    //         req.checkBody('loginOrEmail').trim().isString().isLength({min:3,max:10}).bail()
+    //     }
+    //     return true
+    //
+    // }),
+    check('password').trim().isString().isLength({min:6,max:20}).matches(new RegExp("^[a-zA-Z0-9_-]*$")).bail()
 ]
 
 
